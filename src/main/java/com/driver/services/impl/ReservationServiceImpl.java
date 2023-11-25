5a661b75cb3f6a85d836207a8de798904f1728b1
AnkitKumarAK7 <ankitkumaar1302@gmail.com>
2023-11-25 19:19:23 +0530

    Reservation

--- src/main/java/com/driver/services/impl/ReservationServiceImpl.java
+++ src/main/java/com/driver/services/impl/ReservationServiceImpl.java
@@ -1,31 +1,31 @@
-//package com.driver.services.impl;
-//
-//import com.driver.model.*;
-//import com.driver.repository.ParkingLotRepository;
-//import com.driver.repository.ReservationRepository;
-//import com.driver.repository.SpotRepository;
-//import com.driver.repository.UserRepository;
-//import com.driver.services.ParkingLotService;
-//import com.driver.services.ReservationService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.time.LocalDateTime;
-//import java.util.List;
-//
-//@Service
-//public class ReservationServiceImpl implements ReservationService {
-//    @Autowired
-//    UserRepository userRepository3;
-//    @Autowired
-//    SpotRepository spotRepository3;
-//    @Autowired
-//    ReservationRepository reservationRepository3;
-//    @Autowired
-//    ParkingLotRepository parkingLotRepository3;
-//    @Override
-//    public Reservation reserveSpot(Integer userId, Integer parkingLotId, Integer timeInHours, Integer numberOfWheels) throws Exception {
-//        // Check if the user and parkingLot exist
+package com.driver.services.impl;
+
+import com.driver.model.*;
+import com.driver.repository.ParkingLotRepository;
+import com.driver.repository.ReservationRepository;
+import com.driver.repository.SpotRepository;
+import com.driver.repository.UserRepository;
+import com.driver.services.ParkingLotService;
+import com.driver.services.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class ReservationServiceImpl implements ReservationService {
+    @Autowired
+    UserRepository userRepository3;
+    @Autowired
+    SpotRepository spotRepository3;
+    @Autowired
+    ReservationRepository reservationRepository3;
+    @Autowired
+    ParkingLotRepository parkingLotRepository3;
+    @Override
+    public Reservation reserveSpot(Integer userId, Integer parkingLotId, Integer timeInHours, Integer numberOfWheels) throws Exception {
+        // Check if the user and parkingLot exist
 //        User user = userRepository3.findById(userId)
 //                .orElseThrow(() -> new RuntimeException("User not found with ID: " + userId));
 //
@@ -49,6 +49,8 @@
 //        reservationRepository3.save(reservation);
 //
 //        return reservation;
-//
-//    }
-//}
+
+        return null;
+
+    }
+}
