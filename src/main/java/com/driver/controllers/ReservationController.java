726740a0dc5a33549348a23f474e323bbd93e24f
AnkitKumarAK7 <ankitkumaar1302@gmail.com>
2023-11-25 19:06:07 +0530

    Attempt2

--- src/main/java/com/driver/controllers/ReservationController.java
+++ src/main/java/com/driver/controllers/ReservationController.java
@@ -1,33 +1,34 @@
-package com.driver.controllers;
-
-import com.driver.model.Reservation;
-import com.driver.services.impl.ReservationServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/reservation")
-public class ReservationController {
-    @Autowired
-    ReservationServiceImpl reservationService;
-    @PostMapping("/reserveSpot")
-    public ResponseEntity<Reservation> reserveSpot(@RequestParam Integer userId, @RequestParam Integer parkingLotId, @RequestParam Integer timeInHours, @RequestParam Integer numberOfWheels) throws Exception{
-        //Reserve a spot in the given parkingLot such that the total price is minimum. Note that the price per hour for each spot is different
-        //Note that the vehicle can only be parked in a spot having a type equal to or larger than given vehicle
-        //If parkingLot is not found, user is not found, or no spot is available, throw "Cannot make reservation" exception.
-
-        try {
-            // Reserve a spot
-            Reservation reservation = reservationService.reserveSpot(userId, parkingLotId, timeInHours, numberOfWheels);
-            return new ResponseEntity<>(reservation, HttpStatus.CREATED);
-        } catch (Exception e) {
-            // Handle exceptions and return an appropriate response
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    }
-}
+//package com.driver.controllers;
+//
+//import com.driver.model.Reservation;
+//import com.driver.services.impl.ReservationServiceImpl;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/reservation")
+//public class ReservationController {
+//    @Autowired
+//    ReservationServiceImpl reservationService;
+//    @PostMapping("/reserveSpot")
+//    public ResponseEntity<Reservation> reserveSpot(@RequestParam Integer userId, @RequestParam Integer parkingLotId, @RequestParam Integer timeInHours, @RequestParam Integer numberOfWheels) throws Exception{
+//        //Reserve a spot in the given parkingLot such that the total price is minimum. Note that the price per hour for each spot is different
+//        //Note that the vehicle can only be parked in a spot having a type equal to or larger than given vehicle
+//        //If parkingLot is not found, user is not found, or no spot is available, throw "Cannot make reservation" exception.
+//
+////        try {
+////            // Reserve a spot
+////            Reservation reservation = reservationService.reserveSpot(userId, parkingLotId, timeInHours, numberOfWheels);
+////            return new ResponseEntity<>(reservation, HttpStatus.CREATED);
+////        } catch (Exception e) {
+////            // Handle exceptions and return an appropriate response
+////            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+////        }
+//        return null;
+//    }
+//}
--- src/main/java/com/driver/services/impl/ReservationServiceImpl.java
+++ src/main/java/com/driver/services/impl/ReservationServiceImpl.java
@@ -1,54 +1,54 @@
-package com.driver.services.impl;
-
-import com.driver.model.*;
-import com.driver.repository.ParkingLotRepository;
-import com.driver.repository.ReservationRepository;
-import com.driver.repository.SpotRepository;
-import com.driver.repository.UserRepository;
-import com.driver.services.ParkingLotService;
-import com.driver.services.ReservationService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
-@Service
-public class ReservationServiceImpl implements ReservationService {
-    @Autowired
-    UserRepository userRepository3;
-    @Autowired
-    SpotRepository spotRepository3;
-    @Autowired
-    ReservationRepository reservationRepository3;
-    @Autowired
-    ParkingLotRepository parkingLotRepository3;
-    @Override
-    public Reservation reserveSpot(Integer userId, Integer parkingLotId, Integer timeInHours, Integer numberOfWheels) throws Exception {
-        // Check if the user and parkingLot exist
-        User user = userRepository3.findById(userId)
-                .orElseThrow(() -> new RuntimeException("User not found with ID: " + userId));
-
-        ParkingLot parkingLot = parkingLotRepository3.findById(parkingLotId)
-                .orElseThrow(() -> new RuntimeException("ParkingLot not found with ID: " + parkingLotId));
-
-        // Get the best available spot based on the provided parameters
-        Spot spot =spotService.getBestAvailableSpot(parkingLotId, timeInHours, numberOfWheels);
-
-        if (spot == null) {
-            throw new RuntimeException("No available spot found for reservation");
-        }
-
-        // Create a new reservation entity
-        Reservation reservation = new Reservation();
-        reservation.setUser(user);
-       // reservation.setSpot(spot);
-        reservation.setNumberOfHours(timeInHours);
-
-        // Save the reservation
-        reservationRepository3.save(reservation);
-
-        return reservation;
-
-    }
-}
+//package com.driver.services.impl;
+//
+//import com.driver.model.*;
+//import com.driver.repository.ParkingLotRepository;
+//import com.driver.repository.ReservationRepository;
+//import com.driver.repository.SpotRepository;
+//import com.driver.repository.UserRepository;
+//import com.driver.services.ParkingLotService;
+//import com.driver.services.ReservationService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.time.LocalDateTime;
+//import java.util.List;
+//
+//@Service
+//public class ReservationServiceImpl implements ReservationService {
+//    @Autowired
+//    UserRepository userRepository3;
+//    @Autowired
+//    SpotRepository spotRepository3;
+//    @Autowired
+//    ReservationRepository reservationRepository3;
+//    @Autowired
+//    ParkingLotRepository parkingLotRepository3;
+//    @Override
+//    public Reservation reserveSpot(Integer userId, Integer parkingLotId, Integer timeInHours, Integer numberOfWheels) throws Exception {
+//        // Check if the user and parkingLot exist
+//        User user = userRepository3.findById(userId)
+//                .orElseThrow(() -> new RuntimeException("User not found with ID: " + userId));
+//
+//        ParkingLot parkingLot = parkingLotRepository3.findById(parkingLotId)
+//                .orElseThrow(() -> new RuntimeException("ParkingLot not found with ID: " + parkingLotId));
+//
+//        // Get the best available spot based on the provided parameters
+//        Spot spot =spotService.getBestAvailableSpot(parkingLotId, timeInHours, numberOfWheels);
+//
+//        if (spot == null) {
+//            throw new RuntimeException("No available spot found for reservation");
+//        }
+//
+//        // Create a new reservation entity
+//        Reservation reservation = new Reservation();
+//        reservation.setUser(user);
+//       // reservation.setSpot(spot);
+//        reservation.setNumberOfHours(timeInHours);
+//
+//        // Save the reservation
+//        reservationRepository3.save(reservation);
+//
+//        return reservation;
+//
+//    }
+//}
